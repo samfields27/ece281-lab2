8547b9cb759fb15686b547a38d7aa2f38541db0d
Fields <c27samuel.fields@afacademy.af.edu>
2025-02-25 16:14:34 -0700

    lab2.2

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -39,6 +39,23 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+with i_Hex select
+o_seg_n <=  "0111111" when "0000",
+        "0000110" when "0001"
+        "1011011" when "0010"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when "0000"
+        "0000000" when others;
 
 end Behavioral;
